1a2a4a3cf7025c405c13f0c32990e332ec7d0843
r.wienicke <r.wienicke@connexion.at>
2022-03-10 17:18:23 +0100

    added properties to guest wishlist

--- src/Storefront/Subscriber/WishList.php
+++ src/Storefront/Subscriber/WishList.php
@@ -5,7 +5,7 @@ namespace Melv\PropertiesProductbox\Storefront\Subscriber;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Shopware\Storefront\Page\Wishlist\WishListPageProductCriteriaEvent;
-
+use Shopware\Storefront\Pagelet\Wishlist\GuestWishListPageletProductCriteriaEvent;
 
 class WishList implements EventSubscriberInterface
 {
@@ -21,11 +21,24 @@ class WishList implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return [
-            WishListPageProductCriteriaEvent::class => "handleRequest"
+            WishListPageProductCriteriaEvent::class => "handleRequest",
+            GuestWishListPageletProductCriteriaEvent::class => "handleGuestRequest"
         ];
     }
 
     public function handleRequest(WishListPageProductCriteriaEvent $event): void
+    {
+        $salesChannelId =  $event->getSalesChannelContext()->getSalesChannel()->getId();
+        $active = $this->systemConfigService->get('MelvPropertiesProductbox.config.showWishList', $salesChannelId);
+
+        if(!$active) {
+            return;
+        }
+
+        $event->getCriteria()->addAssociation('properties.group');
+    }
+
+    public function handleGuestRequest(GuestWishListPageletProductCriteriaEvent $event): void
     {
         $salesChannelId =  $event->getSalesChannelContext()->getSalesChannel()->getId();
         $active = $this->systemConfigService->get('MelvPropertiesProductbox.config.showWishList', $salesChannelId);
